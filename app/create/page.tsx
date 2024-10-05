69b6fd492fc628994cda78bcde4db9695f1eb3a1
piyushjha0409 <peeyush0409@gmail.com>
2024-10-05 14:41:32 +0530

    Fix: typo

--- app/create/page.tsx
+++ app/create/page.tsx
@@ -4,9 +4,9 @@
 import { Button } from "@/components/ui/button";
 import { ArrowLeft, Copy, CheckCircle, Loader } from "lucide-react"; // Import CheckCircle icon
 import React, { useState } from "react";
-import { useRouter } from 'next/navigation'
-import { CopyToClipboard } from 'react-copy-to-clipboard';
-import { motion, useAnimation } from 'framer-motion';
+import { useRouter } from "next/navigation";
+import { CopyToClipboard } from "react-copy-to-clipboard";
+import { motion, useAnimation } from "framer-motion";
 import { FaXTwitter } from "react-icons/fa6";
 
 interface TimeSlotFormData {
@@ -21,7 +21,7 @@ interface TimeSlotFormData {
   time2: string;
   time3: string;
   meetlink: string;
-  title: string
+  title: string;
 }
 
 const TimeSlotForm: React.FC = () => {
@@ -37,14 +37,14 @@ const TimeSlotForm: React.FC = () => {
     time2: "",
     time3: "",
     meetlink: "",
-    title: ""
+    title: "",
   });
 
-  const [loader, setLoader] =  useState(false)
+  const [loader, setLoader] = useState(false);
 
   const [imageUrl, setImageUrl] = useState<string | null>(null);
   const [currentStep, setCurrentStep] = useState<number>(1);
-  const router = useRouter()
+  const router = useRouter();
   const [blinkId, setBlinkId] = useState("");
   const [copied, setCopied] = useState(false);
   const [icon, setIcon] = useState(<Copy />); // State to manage the icon
@@ -70,14 +70,13 @@ const TimeSlotForm: React.FC = () => {
     const { name, value } = e.target;
     setFormData({
       ...formData,
-      [name]: name === 'amount' ? Number(value) : value,
+      [name]: name === "amount" ? Number(value) : value,
     });
   };
 
   const handleNavigate = () => {
-    router.push('/dashboard');
+    router.push("/dashboard");
   };
-
 
   const handleFileChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     const file = e.target.files ? e.target.files[0] : null;
@@ -93,13 +92,16 @@ const TimeSlotForm: React.FC = () => {
 
   const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
-    setLoader(true)
+    setLoader(true);
     const formDataToSend = new FormData();
     for (const key in formData) {
-      if (key === 'image' && formData[key]) {
-        formDataToSend.append('image', formData[key]);
+      if (key === "image" && formData[key]) {
+        formDataToSend.append("image", formData[key]);
       } else {
-        formDataToSend.append(key, String(formData[key as keyof TimeSlotFormData]));
+        formDataToSend.append(
+          key,
+          String(formData[key as keyof TimeSlotFormData])
+        );
       }
     }
 
@@ -109,17 +111,12 @@ const TimeSlotForm: React.FC = () => {
         body: formDataToSend,
       });
 
-
-
       if (response.ok) {
-        setLoader(false)
+        setLoader(false);
         const data = await response.json();
-        setBlinkId(data.data.id)
+        setBlinkId(data.data.id);
         // console.log(data.data.id)
-        nextStep()
-        
-        
-        
+        nextStep();
       } else {
         console.error("Error creating time slot");
       }
@@ -140,12 +137,17 @@ const TimeSlotForm: React.FC = () => {
     <div className="flex items-center justify-center h-screen">
       <div className="w-full md:max-w-[500px] p-4">
         <div className="mb-6">
-
-          {currentStep !== 4 && (<>
-          <h1 className="text-3xl font-bold mb-3">Create Your Session Blink</h1>
-          <p className="text-sm text-muted-foreground">Provide all the details below to create a session blink that you can share with your audience. <span className="text-red-500"> otherwise it won&apos;t work. Gonna add zod soon</span></p> 
-          </> )}
-
+          {currentStep !== 4 && (
+            <>
+              <h1 className="text-3xl font-bold mb-3">
+                Create Your Session Blink
+              </h1>
+              <p className="text-sm text-muted-foreground">
+                Provide all the details below to create a session blink that you
+                can share with your audience.
+              </p>
+            </>
+          )}
         </div>
         <form onSubmit={handleSubmit} className="space-y-6">
           {currentStep === 1 && (
@@ -176,7 +178,9 @@ const TimeSlotForm: React.FC = () => {
                 />
               </div>
               <div className="flex flex-col">
-                <label className="mb-2 font-medium text-sm">Solana Publickey</label>
+                <label className="mb-2 font-medium text-sm">
+                  Solana Publickey
+                </label>
                 <input
                   type="text"
                   name="creatorPublicKey"
@@ -316,8 +320,10 @@ const TimeSlotForm: React.FC = () => {
 
           {currentStep === 4 && (
             <div>
-              <h1 className="text-xl font-medium flex items-center gap-1">Here is your blink share it on <FaXTwitter/></h1>
-              
+              <h1 className="text-xl font-medium flex items-center gap-1">
+                Here is your blink share it on <FaXTwitter />
+              </h1>
+
               <div className="px-3 py-2 mt-2 border rounded-lg bg-stone-200 flex items-center justify-between">
                 <p>https://www.minutess.xyz/join/{blinkId}</p>
                 <div onClick={handleCopy} className="cursor-pointer">
@@ -331,43 +337,72 @@ const TimeSlotForm: React.FC = () => {
 
           <div className="flex justify-between">
             {currentStep > 1 ? (
-              <Button type="button" className="rounded-full flex items-center gap-1" variant="outline" onClick={prevStep}>
+              <Button
+                type="button"
+                className="rounded-full flex items-center gap-1"
+                variant="outline"
+                onClick={prevStep}
+              >
                 <ArrowLeft className="w-4" /> Go back
               </Button>
-            ) : <Button disabled type="button" className="rounded-full flex items-center gap-1" variant="outline" onClick={prevStep}>
-              <ArrowLeft className="w-4" /> Go back
-            </Button>}
+            ) : (
+              <Button
+                disabled
+                type="button"
+                className="rounded-full flex items-center gap-1"
+                variant="outline"
+                onClick={prevStep}
+              >
+                <ArrowLeft className="w-4" /> Go back
+              </Button>
+            )}
             {currentStep < 3 && (
-              <Button 
-                type="button" 
-                className="rounded-full" 
-                onClick={nextStep} 
+              <Button
+                type="button"
+                className="rounded-full"
+                onClick={nextStep}
                 disabled={
-                  currentStep === 1 ? !formData.organizationName || !formData.email || !formData.creatorPublicKey || !formData.image :
-                  currentStep === 2 ? !formData.title || !formData.description || !formData.amount || !formData.date :
-                  false
+                  currentStep === 1
+                    ? !formData.organizationName ||
+                      !formData.email ||
+                      !formData.creatorPublicKey ||
+                      !formData.image
+                    : currentStep === 2
+                    ? !formData.title ||
+                      !formData.description ||
+                      !formData.amount ||
+                      !formData.date
+                    : false
                 }
               >
                 Next step
               </Button>
             )}
             {currentStep === 3 && (
-              <Button type="submit" className="rounded-full flex items-center gap-2"
-              disabled={
-                currentStep === 3 ? !formData.time1 || !formData.time2 || !formData.time3 || !formData.meetlink : false
-              }
+              <Button
+                type="submit"
+                className="rounded-full flex items-center gap-2"
+                disabled={
+                  currentStep === 3
+                    ? !formData.time1 ||
+                      !formData.time2 ||
+                      !formData.time3 ||
+                      !formData.meetlink
+                    : false
+                }
               >
-               {
-                loader && <Loader className="animate-spin" />
-               } Create blink
+                {loader && <Loader className="animate-spin" />} Create blink
               </Button>
             )}
-             {currentStep === 4 && (
-              <Button type="button"  onClick={handleNavigate} className="rounded-full">
+            {currentStep === 4 && (
+              <Button
+                type="button"
+                onClick={handleNavigate}
+                className="rounded-full"
+              >
                 Go to dashboard
               </Button>
             )}
-            
           </div>
         </form>
       </div>
